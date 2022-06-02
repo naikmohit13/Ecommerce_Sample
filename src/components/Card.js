3741cf4f30b3e2c3e536238cb69b18cbe62c2315
naikmohit13 <mmnaik42@gmail.com>
2022-06-02 23:49:29 +0530

    added card props ans used row-col layout in Men component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,17 @@
 import React from 'react'
-
-export default function Card() {
+import {Link} from 'react-router-dom'
+export default function Card(props) {
   return (
-    <div className="card" style="width: 18rem;">
-  <img src="..." className="card-img-top" alt="..."/>
+    <div className="card shadow" style={{ width: "18rem" }}>
+      <div className="card_image">
+      <img src={props.img_url} className="card-img-top" alt="..."/>
+      
+      </div>
   <div className="card-body">
-    <h5 className="card-title">Card title</h5>
+    <h5 className="card-title">{props.title}</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
+    {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
+    <Link to='/' className="btn btn-sm btn-primary shadow-sm">Go somewhere</Link>
   </div>
 </div>
   )
--- src/components/Men.js
+++ src/components/Men.js
@@ -3,11 +3,28 @@ import Card from './Card'
 
 export default function Men() {
   return (
-    <>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-    </>
+    <div className="container flex" id="men-content">
+      <div className='row'>
+        <div class='col'>
+        <Card title="Shirts" img_url="https://nypost.com/wp-content/uploads/sites/2/2022/03/91TIoZtb-L._AC_SX679._SX._UX._SY._UY_.jpg?quality=90&strip=all"/>
+        </div>
+        <div class='col'>
+        <Card title="Pants" img_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVrj247uDZbRnCEliPseJrrgBq1kqL1bUDaw&usqp=CAU"/>
+        </div>
+        <div class='col'>
+        <Card title="Shoes" img_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQrAibxrtXx5PqvW4bDl_qTeAa9J-BDKY1Nw&usqp=CAU"/>
+        </div>
+        <div class='col'>
+        <Card title="Watches" img_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsdCQE68jhfV8U5tZ_tY_jnLI_lMV_7nP6SQ&usqp=CAU"/>
+        </div>
+        <div class='col'>
+        <Card title="Sunglassess" img_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsdCQE68jhfV8U5tZ_tY_jnLI_lMV_7nP6SQ&usqp=CAU"/>
+        </div>
+        <div class='col'>
+        <Card title="Perfumes" img_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsdCQE68jhfV8U5tZ_tY_jnLI_lMV_7nP6SQ&usqp=CAU"/>
+        </div>
+        
+      </div>
+    </div>
   )
 }
